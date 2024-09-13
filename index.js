640e3477c315f2474f821022752c16bb3b75c233
MuhammadAbdullahWarraich <muhammadabdullah2016pk@gmail.com>
2024-09-13 18:39:49 +0500

    add edit and hide details features for todo item

--- index.js
+++ index.js
@@ -7,7 +7,47 @@ document.querySelector('form').addEventListener('submit', (e) => {
         <p>Due: ${edottarget[2].value}</p>
         <p>Priority: ${edottarget[3].value}</p>
     `;
+    item.classList.add('state-0');
     document.querySelector('ul').appendChild(item);
+    item.addEventListener('click', (event) => {
+        const et = event.target;
+        const etp = et.parentElement;
+        if (etp.classList.contains('state-0')) {
+            etp.classList.remove('state-0');
+            etp.classList.add('state-1');
+        } else if (etp.classList.contains('state-1')) {
+            etp.classList.remove('state-1');
+            etp.classList.add('state-2');
+            for (let i = 0; i < etp.children.length; i++) {
+                const val = etp.children[i].innerHTML;
+                let input = "";
+                if (i == 2) {
+                    input = `<input type="date" value="${val.slice(5)}">`;
+                }
+                // } else if (i == 3) {
+                //     input = `<input type="text" value = "${val.slice(10)}"`;
+                // } 
+                else {
+                    input = `<input type="text" value="${val}">`;
+                }
+                etp.children[i].innerHTML = input;
+            }
+        } else if (etp.classList.contains('state-2')) {
+            etp.classList.remove('state-2');
+            etp.classList.add('state-0');
+            for (let i = 0; i < etp.children.length; i++) {
+                console.log(etp.children[i].children[0].value);
+                let input = etp.children[i].children[0].value;
+                if (i == 2) {
+                    input = "Due: " + input;
+                }
+                // } else if (i == 3) {
+                //     input = "Priority: " + input;
+                // }
+                etp.children[i].innerHTML = input;
+            }
+        }
+    });
     toggleForm();
     e.target.reset();
     e.preventDefault();
